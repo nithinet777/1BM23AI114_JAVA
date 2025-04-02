f24f55393cef3c7d46cf600ad5081f0462e1f4dd
nithinet777 <nithinet.ai23@bmsce.ac.in>
2025-04-02 14:44:28 +0530

    Add files via upload

---
+++ enumeration.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class enumeration {
+    public enum Daysofweek{
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY;
+
+        public boolean isWorkday(){
+            return(this!=SATURDAY && this!=SUNDAY);
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("is monday a work day?"+Daysofweek.MONDAY.isWorkday());
+        System.out.println("is saturday is a workday?"+Daysofweek.SATURDAY.isWorkday());
+    }
+}
